588fcda495c2b76f37c6bc98e87eeefca2642523
Double327 <203814477@qq.com>
2020-04-04 21:47:40 +0800

    修改代码格式

--- src/com/jsnu/pms/controller/HistroyFrame.java
+++ src/com/jsnu/pms/controller/HistroyFrame.java
@@ -12,21 +12,21 @@ public class HistroyFrame extends JFrame {
 
     private JTable table;
 
-    public HistroyFrame(){
+    public HistroyFrame() {
 
-        String[] columnName = {"卡号","车牌号","开始时间","结束时间","总时","费用"};
+        String[] columnName = {"卡号", "车牌号", "开始时间", "结束时间", "总时", "费用"};
         Object[][] rowData = new Object[6][6];
 
 
-       //获取窗口容器
+        //获取窗口容器
         Container container = getContentPane();
         //实列化表格
-        table = new JTable(rowData,columnName);
+        table = new JTable(rowData, columnName);
         //增加组件
-        container.add(new JScrollPane(table),BorderLayout.CENTER);
+        container.add(new JScrollPane(table), BorderLayout.CENTER);
 
 
-        setSize(600,600);
+        setSize(600, 600);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
