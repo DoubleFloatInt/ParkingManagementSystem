06956ef75745860c34217add39927af9631a5038
Universe110 <2962932840@qq.com>
2020-04-04 20:58:54 +0800

    历史记录窗口，历史记录读取，预约记录窗口

---
+++ src/com/jsnu/pms/controller/OrderFrame.java
@@ -0,0 +1,17 @@
+package com.jsnu.pms.controller;
+
+import javax.swing.*;
+
+/**
+ * @Author 周星晨
+ * @Date 2020/4/4 20:39
+ */
+public class OrderFrame extends JFrame {
+
+    private JTable table;
+
+    public OrderFrame(){
+        String[] columnName = {"卡号","车牌号","开始时间"};
+        Object[][] rowData = new Object[10][4];
+    }
+}
