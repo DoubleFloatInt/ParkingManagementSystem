e2fcac89cb5f791007d365c3ab71586bf5ffdc9f
Double327 <203814477@qq.com>
2020-04-04 21:00:31 +0800

    部分界面及服务层代码、持久层接口

--- src/com/jsnu/pms/dao/ICarDao.java
+++ src/com/jsnu/pms/dao/ICarDao.java
@@ -2,11 +2,37 @@ package com.jsnu.pms.dao;
 
 import com.jsnu.pms.entity.Car;
 
+import java.util.List;
+
 /**
  * @author 李广帅
  * @Date 2020/4/4 6:15 下午
  */
 public interface ICarDao {
+
+
+    /**
+     * 获取所有车辆信息
+     * @return
+     */
+    public List<Car> getAllCars();
+
+
+    /**
+     * 获取停车数量
+     * @return
+     */
+    public Integer getCarsNum();
+
+
+    /**
+     * 获取空位数量
+     * @return
+     */
+    public Integer getEmptyCarportNum();
+
+
+
     /**
      * 添加停车信息
      * @param car
@@ -16,8 +42,23 @@ public interface ICarDao {
 
     /**
      * 删除停车信息
+     * @param licensePlateNumber
      */
-    public void deleteCar();
+    public void deleteCar(String licensePlateNumber);
+
+
+    /**
+     * 添加预约
+     * @param car
+     */
+    public void addAppointment(Car car);
+
+
+    /**
+     * 删除预约
+     * @param licensePlateNumber
+     */
+    public void deleteAppointment(String licensePlateNumber);
 
 
 
--- src/com/jsnu/pms/dao/impl/CarDaoImpl.java
+++ src/com/jsnu/pms/dao/impl/CarDaoImpl.java
@@ -3,13 +3,33 @@ package com.jsnu.pms.dao.impl;
 import com.jsnu.pms.dao.ICarDao;
 import com.jsnu.pms.entity.Car;
 
+import java.util.List;
+
 /**
  * @author 李广帅
  * @Date 2020/4/4 6:32 下午
  */
 public class CarDaoImpl implements ICarDao {
+    private Car head;
 
+    public CarDaoImpl() {
+        // 初始化链表
+    }
 
+    @Override
+    public List<Car> getAllCars() {
+        return null;
+    }
+
+    @Override
+    public Integer getCarsNum() {
+        return null;
+    }
+
+    @Override
+    public Integer getEmptyCarportNum() {
+        return null;
+    }
 
     @Override
     public void addCar(Car car) {
@@ -17,7 +37,18 @@ public class CarDaoImpl implements ICarDao {
     }
 
     @Override
-    public void deleteCar() {
+    public void deleteCar(String licensePlateNumber) {
 
     }
+
+    @Override
+    public void addAppointment(Car car) {
+
+    }
+
+    @Override
+    public void deleteAppointment(String licensePlateNumber) {
+
+    }
+
 }
